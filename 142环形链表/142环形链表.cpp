a52ddce3c030fe0f76591ca646f8a0bc5b6dee00
lgtnice <1798607710@qq.com>
2022-05-17 10:35:47 +0800

    5.17

---
+++ 142环形链表/142环形链表.cpp
@@ -0,0 +1,43 @@
+﻿// 142环形链表.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* detectCycle(ListNode* head) {
+    //空链表，为特殊情况，加代码
+    if (head == nullptr) { return nullptr; }
+    ListNode* aim = head;
+
+    aim = aim->next;
+    while (aim != nullptr)
+    {
+        ListNode* temp = head;
+        while (temp!=aim)
+        {
+            //用的笨比方法确认存在环，时间复杂度 n的平方
+            if (temp == aim->next)
+            {
+                return temp;
+            }
+            temp = temp->next;
+        }
+        //最后的一个结点的next指向它自己，这是一种特殊情况，要加代码
+        if (aim == aim->next) { return aim; }
+        aim = aim->next;
+    }
+    return nullptr;
+}
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
---
+++ 242有效的字母异位词/242有效的字母异位词.cpp
@@ -0,0 +1,32 @@
+﻿// 242有效的字母异位词.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isAnagram(string s, string t) {
+	if (s.length() != t.length())
+	{
+		return false;
+	}
+	int temp[26] = {0};
+	for (char c:s)
+	{
+		temp[c - 'a']++;
+	}
+	for (char c: t)
+	{
+		if (--temp[c - 'a'] < 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
