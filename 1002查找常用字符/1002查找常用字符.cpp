0a6caea9b303fabad40f97af9793e4897827a5e7
lgtnice <1798607710@qq.com>
2022-05-30 19:08:15 +0800

    5.30

--- 1002查找常用字符/1002查找常用字符.cpp
+++ 1002查找常用字符/1002查找常用字符.cpp
@@ -1,14 +1,44 @@
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
 
+vector<string> commonChars(vector<string>& words) {
+    vector<string> res;
+    for (char ch : words[0])
+    {
+        bool success = true;
+        vector<int> temp(words.size());
+        for (short i = 1; i < words.size(); i++)
+        {
+            int index = words[i].find(ch);
+            temp[i] = index;
+            if (index == -1) { success = false; break; }
+        }
+        if (success)
+        {
+            char c[]  = {ch, '\0'};
+            res.push_back(c);
+            for (short i = 1; i < words.size(); i++)
+            {
+                words[i] = words[i].erase(temp[i], 1);
+            }
+        }
+    }
+    return res;
+}
+
 int main()
 {
+    vector<string> words = {"bella","label","roller"};
+    vector<string> str = commonChars(words);
     std::cout << "Hello World!\n";
 }
 
-bool existInArray(string s)
+
+bool existInArray(char ch, string s)
 {
-    return true;
+    unordered_set<char> st(s.begin(),s.end());
+	return st.find(ch) != st.end();
 }
